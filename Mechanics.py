de51b799ad92a2cd46cba8c0bb13f428ede7572a
fourierlaplace124-design <fourierlaplace124@gmail.com>
2025-12-03 17:53:06 +0800

    Add files via upload

---
+++ Mechanics.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy.integrate import quad
+
+class Mechanics: 
+    def Sliding_dist(L1,L2,theta_1,theta_2):
+        if theta_1 and theta_2 >= 0 and theta_1 and theta_2 <= 90:
+            tan_1 = np.tan(np.radians(theta_1))
+            tan_2 = np.tan(np.radians(theta_2))
+            cot_1 = 1/tan_1
+            d = L2 - L1*cot_1
+            if d >= 0:
+                real_d = d
+            else:
+                print("No sliding occurs")
+                real_d = 0
+        else:
+            raise ValueError("Angles must be between 0 and 90 degrees")
+        return real_d
+    
+    def theta(T):
+    # 先对 d_Omega_d_t 积分得到 Omega
+        def omega_func(t):
+            return quad(d_Omega_d_t, 0, t)[0]
+        # 再对 Omega 积分得到角位移
+        theta_val, _ = quad(omega_func, 0, T)
+        return theta_val
+
+    def omega_func(t):
+        return quad(d_Omega_d_t, 0, t)[0]
+    
+    
